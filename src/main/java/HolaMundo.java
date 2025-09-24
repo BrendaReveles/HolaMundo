f6d448196941e9cd46ae7e9c0ff4fa70abfb3a71
brend <brend@192.168.0.149>
2025-09-24 07:07:07 -0600

    inicial

---
+++ src/main/java/HolaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author brend
+ */
+public class HolaMundo {
+    public  static void main (String [] args) {
+        System.out.println("Hola mundo");
+        areaRectangulo(2,5);
+        
+    }
+            public static void areaRectangulo(int a, int b){
+                System.out.println("area:"+(a*b));
+            }
+}
